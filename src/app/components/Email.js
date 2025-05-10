650b61bfb4f0f0763b8274303810dc54f4530f2c
Rahat Bano <160172836+RahatBano58@users.noreply.github.com>
2025-05-11 00:30:48 +0500

    Update Email.js

--- src/app/components/Email.js
+++ src/app/components/Email.js
@@ -5,16 +5,14 @@ import { Mail } from 'lucide-react'
 export default function Email() {
   return (
     <div className="bg-gray-100 py-10 text-center px-4">
-      <h1 className="text-lg font-semibold">Subscribe on our newsletter</h1>
-      <p className="text-gray-500 mb-4">
+      <h1 className="text-lg font-semibold sm:text-xl md:text-2xl">Subscribe to our newsletter</h1>
+      <p className="text-gray-500 mb-4 sm:text-sm md:text-base">
         Get daily news on upcoming offers from many suppliers all over the world
       </p>
 
-      <div className="flex justify-center items-center gap-2 max-w-md mx-auto">
+      <div className="flex flex-col sm:flex-row justify-center items-center gap-4 max-w-md mx-auto">
         <div className="relative w-full">
-    
           <Mail className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
-
           <input
             type="email"
             placeholder="Email"
@@ -22,8 +20,7 @@ export default function Email() {
           />
         </div>
 
-   
-        <button className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition">
+        <button className="w-full sm:w-auto bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition">
           Subscribe
         </button>
       </div>
